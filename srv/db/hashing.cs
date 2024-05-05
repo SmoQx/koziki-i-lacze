c29b92d24ac599313745eebd12532126d9e55413
SmoQx <smoqxpl@gmail.com>
2024-05-05 13:59:31 +0200

    added a table class for user creds, and hashing alg

---
+++ srv/db/hashing.cs
@@ -0,0 +1,19 @@
+using System.Security.Cryptography;
+using System.Text;
+
+public static class Hashing
+{
+    public static string ComputeSHA256Hash(string input)
+    {
+        using (SHA256 sha256 = SHA256.Create())
+        {
+            byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(input));
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                builder.Append(bytes[i].ToString("x2"));
+            }
+            return builder.ToString();
+        }
+    }
+}
