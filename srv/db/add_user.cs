e50ed7373e2901848bf56b0d7556bba44149b69f
SmoQx <smoqxpl@gmail.com>
2024-05-06 08:33:41 +0200

    created credential adder

--- srv/db/add_user.cs
+++ srv/db/add_user.cs
@@ -1,8 +1,8 @@
 using SQLite;
 
-public class Add_player
+public class Add_User
 {
-    public static void add_player()
+    public static void add_user()
     {
         // Specify the path to the SQLite database file
         string databasePath = "MyDatabase.db";
@@ -11,25 +11,21 @@ public class Add_player
         using (SQLiteConnection connection = new SQLiteConnection(databasePath))
         {
             // Create the UserData table (if it doesn't exist)
-            connection.CreateTable<UserData>();
+            connection.CreateTable<User_credentials>();
 
             // Insert sample data into the UserData table
-            var user = new UserData
+            var user = new User_credentials
             {
-                Nickname = "Player1",
-                Level = 10,
-                ItemsList = "Sword, Shield, Potion",
-                HP = 100,
-                Mana = 50,
-                Skills = "Attack, Defense, Heal"
+                UserName = "usr",
+                PasswordHash = "password"
             };
             connection.Insert(user);
 
             // Retrieve all data from the UserData table
-            var allUsers = connection.Table<UserData>().ToList();
+            var allUsers = connection.Table<User_credentials>().ToList();
             foreach (var u in allUsers)
             {
-                Console.WriteLine($"ID: {u.Id}, Nickname: {u.Nickname}, Level: {u.Level}, Items: {u.ItemsList}, HP: {u.HP}, Mana: {u.Mana}, Skills: {u.Skills}");
+                Console.WriteLine($"user name: {u.UserName}, password: {u.PasswordHash}");
             }
 
             // Drop the UserData table (for cleanup, you may omit this)
