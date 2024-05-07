790762c8a8bc77f9f11e42a274e3c07bd1fac558
SmoQx <smoqxpl@gmail.com>
2024-05-07 13:04:46 +0200

    simplified adders and concatinated readers to one class

--- srv/db/Program.cs
+++ srv/db/Program.cs
@@ -2,8 +2,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Read_users.read_users();
-        Read_user_data.read_user_data();
-        Read_scoreboard.read_scoreboard();
+    var userData = new UserData
+        {
+            Nickname = "nickname",
+            Level = 10,
+            ItemsList = "itemsList",
+            HP = 10,
+            Mana = 20,
+            Skills = "skills"
+        };
+        var user = new User_credentials
+        {
+            UserName = "asdfusrsdf",
+            PasswordHash = "asdpaasdfssword"
+        };
+        var player = new PlayerData
+        {
+            Pozycja = 4,
+            Nick = "Name",
+            PoziomDoswiadczenia = 100,
+            Zwyciestwa = 100,
+            Porazki = 20,
+            Ratio = 5,
+            CzasGry = "50h"
+        };
+
+        Adder.Add(userData);
+        Adder.Add(user);
+        Adder.Add(player);
+        Readers.read_user_data();
+        Readers.read_users();
+        Readers.read_scoreboard();
     }
 }
